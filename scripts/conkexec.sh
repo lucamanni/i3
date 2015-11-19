38675cadc21bf41a369a311e82d66029b01d4809
Luca Manni <luca.manni.88@gmail.com>
2015-11-19 10:28:10 +0100

    Updated

--- scripts/conkexec.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$*
-killall -sUSR1 conky
--- scripts/screenshot.sh
+++
@@ -1,4 +0,0 @@
-!#/bin/bash
-scrot '%F--%I:%M:%S:%p--$wx$h--scrot.png' -e 'mv $f /home/luca/Immagini/Screenshots/'
-notify-send -t 500 -a Scrot "Screenshot Taken"
-sleep 2 && pcmanfm /home/luca/Immagini/Screenshots
